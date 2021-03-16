a73179b45d9329071ab45814a45fdd7a26fea638
Shaun Inman <shaun@panic.com>
2021-03-16 18:15:31 -0400

    drop clock speed to 16MHz(!) on sleep

--- mmenu.c
+++ mmenu.c
@@ -96,6 +96,7 @@ static void initTrimuiAPI(void) {
 	InitKeyShm(key);
 }
 
+#define kCPUDead 0x0112 // 16MHz (dead)
 #define kCPULow 0x00c00532 // 192MHz (lowest)
 #define kCPUNormal 0x02d01d22 // 720MHz (default)
 #define kCPUHigh 0x03601a32 // 864MHz (highest stable)
@@ -128,7 +129,7 @@ static void fauxSleep(void) {
 	// printf("before v:%i b:%i\n",v,b);
 	setVolume(0);
 	setBrightness(0);
-	setCPU(kCPULow);
+	setCPU(kCPUDead);
 	
 	SDL_Event event;
 	int wake = 0;
@@ -413,7 +414,7 @@ MenuReturnStatus ShowMenu(char* rom_path, char* save_path_template, SDL_Surface*
 			}
 		}
 		
-		#define kSleepDelay 60000 // 1m
+		#define kSleepDelay 30000
 		if (pressed_menu || SDL_GetTicks()-cancel_start>=kSleepDelay) {
 			SDL_FillRect(buffer, NULL, 0);
 			SDL_BlitSurface(buffer, NULL, screen, NULL);
